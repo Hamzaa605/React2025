f6d9c3eac00f95d2599c843978a05c9f7cab2eb3
shankkarpal46 <shankkarpal46@gmail.com>
2025-01-07 09:02:38 +0530

    4 commit

--- second-app/src/App.js
+++ second-app/src/App.js
@@ -1,13 +1,57 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import About from './components/About';
+import Contant from './components/Contant';
+import Footer from './components/Footer';
+import NotFound from './components/NotFound';
+
+
+const router = createBrowserRouter([
+  {
+
+    path: "/about",
+    element: <>
+    <Navbar />
+    <About/>
+    <Footer />
+    </>
+
+  },
+  {
+
+    path: "/contact",
+    element:  <>
+    <Navbar />
+    <Contant/>
+    <Footer />
+    </>
+
+
+  },
+  {
+    path: "/",
+    element: <>
+      <Navbar />
+      <Footer />
+    </>
+  },
+
+  {
+    path: "*",
+    element:
+    <NotFound/>
+  }
+])
 
 
 function App() {
   return (
     <div >
-     <Navbar/>
-        
+
+      {<RouterProvider router={router} />}
+
 
     </div>
   );
